1ee785157b482c19131a90658250eee3a8b700d9
awsmit07 <awsmit@shaw.ca>
2023-03-03 12:10:03 -0700

    Lab 1 work to question 7

--- Lab_1/Core/Src/main.c
+++ Lab_1/Core/Src/main.c
@@ -20,9 +20,11 @@
 #include "main.h"
 #include "usb_host.h"
 
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "arm_math.h"
+#include <inttypes.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -48,7 +50,56 @@ I2S_HandleTypeDef hi2s3;
 SPI_HandleTypeDef hspi1;
 
 /* USER CODE BEGIN PV */
+/* This is the start address of where we are going to put our input data */
+#define DATA_START_ADDRESS	0x08020000
 
+/* For this lab, we should know in advance how many samples we have to process... */
+#define NUMBER_OF_SAMPLES	22050
+
+/* This is a pointer to our input data */
+float32_t* data = DATA_START_ADDRESS;
+
+/* Here are our filter taps (as float32_t) */
+static float32_t filter_taps[31] = {
+  -0.004289844297235351,
+  -0.00014156432081732112,
+  -0.0036201686224482756,
+  0.004827257985150652,
+  0.0036155531164272516,
+  0.020675517860072025,
+  0.02402285521584296,
+  0.051424412549522944,
+  0.05939430591101754,
+  0.09527637902148803,
+  0.10443208231700706,
+  0.14294541181873402,
+  0.14707868991005837,
+  0.18028398272481666,
+  0.17316624396732433,
+  0.19445415344945507,
+  0.17316624396732433,
+  0.18028398272481666,
+  0.14707868991005837,
+  0.14294541181873402,
+  0.10443208231700706,
+  0.09527637902148803,
+  0.05939430591101754,
+  0.051424412549522944,
+  0.02402285521584296,
+  0.020675517860072025,
+  0.0036155531164272516,
+  0.004827257985150652,
+  -0.0036201686224482756,
+  -0.00014156432081732112,
+  -0.004289844297235351
+};
+
+/* Here are our filter taps (as int16_t or q15_t) */
+//static int16_t filter_taps[31] = {-141, -5, -119, 158, 118, 677, 787, 1685, 1946, 3122, 3422, 4684, 4819, 5908, 5674, 6372, 5674, 5908, 4819, 4684, 3422, 3122, 1946, 1685, 787, 677, 118, 158, -119, -5, -141};
+
+/* let's have our history and output arrays defined somewhere */
+float32_t history[31];
+float32_t newdata[NUMBER_OF_SAMPLES];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -62,7 +113,14 @@ static void MX_SPI1_Init(void);
 void MX_USB_HOST_Process(void);
 
 /* USER CODE BEGIN PFP */
+/* this the function prototype for initializing the filter (specifically the history) */
+void FloatFilterInit(void);
 
+/* This is the function prototype that does the filtering given a new sample.
+ * The filter should shuffle the history values (which it can because history is global
+ * This function should return a new output
+ */
+float32_t FloatFilterGet(float new_sample);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -75,6 +133,67 @@ int _write(int file, char* ptr, int len)
 		ITM_SendChar(*ptr++);
 	}
 	return len;
+}
+
+
+void BasicLoopTest()
+{
+  for(uint32_t i=0; i < 2000000; i++);
+}
+
+void FloatingExperiment()
+{
+  float a = 10102.1;
+  float b = 0.99;
+  float c = a * b;
+}
+
+// Q1.15 fixed point numbers
+void FixedExperiment()
+{
+  int16_t a = 0x4000;
+  int16_t b = 0x1000;
+  int16_t c = ((int32_t)a * (int32_t)b) >> 15;
+}
+
+void CMSISExperiment(void)
+{
+	float32_t float_array[10] = {-1, 0.1, 0.25, 0.99, 0, 100.15, 2, 8, 0.9, -0.75};
+	q15_t q15_array[10] = {};
+
+	arm_float_to_q15(float_array, q15_array, 10);
+}
+
+void FloatFilterInit(void)
+{
+	for (int i = 30; i > 0; i--)
+	{
+		history[i] = *data;
+		data++;
+	}
+
+	return;
+}
+
+/* here is the function that does the filtering */
+float32_t FloatFilterGet(float32_t newsample) {
+	// set the new sample as the head
+	history[0] = newsample;
+
+	float32_t accumulator = 0;
+	// set up and do our convolution
+	for(int i = 0; i < 31; i++)
+	{
+		accumulator += history[i] * filter_taps[i];
+	}
+
+	// shuffle the history along for the next one?
+	for(int i = 1; i < 31; i++)
+	{
+		history[i] = history[i-1];
+	}
+
+	return accumulator;
 }
 /* USER CODE END 0 */
 
@@ -115,8 +234,17 @@ int main(void)
   MX_SPI1_Init();
   MX_USB_HOST_Init();
   /* USER CODE BEGIN 2 */
-
+//  printf("Andy Smit, Colton Giesbrecht, 2023-03-02, Why did the DSP Engineer get stuck in the revolving door? Because he couldn't stop convolving!\n");
   HAL_SuspendTick();
+
+  ITM_Port32(31) = 1;
+  CMSISExperiment();
+  ITM_Port32(31) = 2;
+
+  for(int i=30; i < NUMBER_OF_SAMPLES; i++)
+  {
+	  newdata[i] = FloatFilterGet(*(data++));
+  }
   /* USER CODE END 2 */
 
   /* Infinite loop */
